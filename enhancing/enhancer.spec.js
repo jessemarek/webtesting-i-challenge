3717e49bbe18b990d9283b47974a19a6f05349a2
unknown <dethgrenade@gmail.com>
2020-06-17 12:15:37 -0700

    reapir item test done

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,27 @@
 const enhancer = require('./enhancer.js');
+const { repair } = require('./enhancer.js');
 // test away!
+describe('item enhancer', () => {
+    it('runs tests', () => {
+        expect(true).toBe(true)
+    })
+
+    describe('test repair(item)', () => {
+        it('returns new item with durablitiy set to 100', () => {
+            const item = {
+                name: 'item',
+                durability: 35,
+                enhancement: 0
+            }
+
+            const repairedItem = {
+                name: 'item',
+                durability: 100,
+                enhancement: 0
+            }
+
+            expect(repair(item)).not.toBe(item)
+            expect(repair(item)).toEqual(repairedItem)
+        })
+    })
+})
